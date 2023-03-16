6ac1a7f2f6e2a0b4ce5882b1424df920d9bb93df
danila <danila.kononov@netcracker.com>
2023-03-17 00:07:31 +0500

    2 H/W 3

--- Hw_3_1.java
+++ Hw_3_1.java
@@ -1,19 +1,24 @@
-import java.util.List;
-import java.util.stream.Collectors;
-import java.util.stream.Stream;
-
-class Good {
-    public String name;
-    public Integer price;
-    public String sort;
-
-    static Good [] generate  () {
-     /*   List<Good> goods = Stream.iterate(1, n -> new Good())
-                .limit(10)
-                .collect(Collectors.toList());
-        return goods;*/
-    }
-}
+import java.util.ArrayList;
+import java.util.Comparator;
 
 public class Hw_3_1 {
+
+    private ArrayList<Good> getGoodList() {
+        ArrayList<Good> goods = new ArrayList<Good>();
+        goods.add(new Good("Высший 1", 1143, 2));
+        goods.add(new Good("Good 2", 1235, 2));
+        goods.add(new Good("Высший 3", 234, 1));
+        goods.add(new Good("Good 4", 123, 2));
+        goods.add(new Good("Высший 5", 52, 2));
+        goods.add(new Good("Good 6", 242, 1));
+        goods.add(new Good("Высший 6", 222, 1));
+        goods.add(new Good("Высший 6", 1142, 2));
+        return goods;
+    }
+    public void findHighestPrice() {
+        ArrayList<Good> goods = this.getGoodList();
+        int highestPrice = goods.stream().filter(x -> x.name.contains("Высший") && (x.sort == 1 || x.sort == 2))
+                                .max(Comparator.comparing(Good::getPrice)).get().getPrice();
+        System.out.println(highestPrice);
+    }
 }
--- Hw_3_2.java
+++ Hw_3_2.java
@@ -1,2 +1,27 @@
+import java.util.ArrayList;
+import java.util.Comparator;
+import java.util.List;
+import java.util.Map;
+import java.util.stream.Collectors;
+
 public class Hw_3_2 {
+
+    private ArrayList<Good> getGoodList() {
+        ArrayList<Good> goods = new ArrayList<Good>();
+        goods.add(new Good("Высший 1", 1143, 2, "sss", 345));
+        goods.add(new Good("Good 2", 1235, 2, "sss", 345));
+        goods.add(new Good("Высший 3", 234, 1, "sss", 345));
+        goods.add(new Good("Good 4", 123, 2, "sss", 345));
+        goods.add(new Good("Высший 5", 52, 2, "sss", 345));
+        goods.add(new Good("Good 6", 242, 1, "sss", 345));
+        goods.add(new Good("Высший 6", 222, 1, "sss", 345));
+        goods.add(new Good("Высший 6", 1142, 2, "sss", 345));
+        return goods;
+    }
+
+    public ArrayList<String> getNameOfMinGoodsBySort(int sort) {
+        ArrayList<Good> goods = this.getGoodList();
+        Map<Integer, List<Good>> groups = goods.stream().filter(x -> x.sort == sort).collect(Collectors.groupingBy(Good::getSort));
+        return groups.values().stream().min(Comparator.comparing(Good::getPrice)).get().stream().map(x -> x.getName());
+    }
 }
