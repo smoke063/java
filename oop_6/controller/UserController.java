7989ac01a9d84492886c0f0b127ee0c08b254011
danila <danila.kononov@netcracker.com>
2023-10-21 18:07:32 +0400

    - remove

--- oop_6/controller/UserController.java
+++
@@ -1,9 +0,0 @@
-package oop_6.controller;
-
-import oop_6.data.User;
-
-import java.time.LocalDate;
-
-public interface UserController<T extends User> {
-    void create(String firstName, String secondName, String patronymic, LocalDate dateOfBirth);
-}
--- oop_6/view/UserView.java
+++
@@ -1,9 +0,0 @@
-package oop_6.view;
-
-import oop_6.data.User;
-
-import java.util.List;
-
-public interface UserView<T extends User> {
-    void sendOnConsole(List<T> list);
-}
